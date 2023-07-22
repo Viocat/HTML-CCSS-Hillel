a694f0b9f77116cba1a9c32a60e893defd0f1551
Violetta <violett.vitacat@gmail.com>
2023-07-22 15:11:37 +0300

    zxczxc

--- js/index.js
+++ js/index.js
@@ -5,6 +5,9 @@
     const form=document.querySelector("form");
     const enterBlock=document.querySelector('.entrance');
     const registerBlock=document.querySelector('.registration');
+    const password=document.querySelector('#user-password')
+    const eyeOnSvg=document.querySelector('.eye-on-icon');
+    const eyeOffSvg=document.querySelector('.eye-off-icon');
 
     form.addEventListener('change',(event)=>{
         event.preventDefault();
@@ -15,6 +18,19 @@
             registerBlock.classList.toggle('hidden');
         }
 
+
+    })
+
+    form.addEventListener('click', (event)=>{
+        event.preventDefault()
+        event.stopPropagation();
+        const {target}=event;
+        console.log(target);
+        if(target===eyeOnSvg||target===eyeOffSvg){
+            eyeOnSvg.classList.toggle('hidden');
+            eyeOffSvg.classList.toggle('hidden');
+            eyeOnSvg.classList.contains('hidden')?password.type='text':password.type='password';
+        }
 
     })
 
